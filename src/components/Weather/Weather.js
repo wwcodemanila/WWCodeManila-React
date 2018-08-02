8aa405281f09cfe100545f1db3c72292e0d3b0bd
ksmorano <ksmorano@gmail.com>
2018-08-02 19:38:09 +0800

    update: weather
    add table component
    add weather data table

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import PageContent from "components/Elements/PageContent";
+import Table from "components/Weather/Table";
+
 class Weather extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      weatherData: []
+    };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     axios
-      .get("https://noah.up.edu.ph/api/four_hour_forecast")
-      .then(results => console.log(results.data))
-      .catch(error => console.log(error));
+      .get(
+        "https://us-central1-terminus-161218.cloudfunctions.net/noah/four_hour_forecast"
+      )
+      .then(response => {
+        this.setState({
+          weatherData: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
-    return <div>weather here</div>;
+    const headers = ["Location", "Longitude", "Latitude", "Hourly Forecast"];
+    return this.state.weatherData.length === 0 ? (
+      <h3 className="subtitle has-text-centered">Loading...</h3>
+    ) : (
+      <PageContent
+        title="Weather Forecast"
+        description="Data taken from NOAH 4 Hour Forecast"
+      >
+        <Table headers={headers} data={this.state.weatherData} />
+      </PageContent>
+    );
   }
 }
 
