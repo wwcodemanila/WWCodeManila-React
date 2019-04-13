0609f4bf3cc56bef3f857c4aa3030a4eec6fa5ca
katefloresbanez <katefloresbanez@gmail.com>
2019-04-13 08:38:59 +0800

    added action files for products page

---
+++ sample-app/src/actions/userActions.js
@@ -0,0 +1,13 @@
+
+export const UPDATE_USER = 'users:updateUser'
+
+
+export function updateUser (newUser) {
+  return {
+    type: UPDATE_USER,
+    payload: {
+      user: newUser
+    }
+  }
+}
+
