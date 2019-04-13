26046bd10c438a4763ec71f888284147c9f95ab2
katefloresbanez <katefloresbanez@gmail.com>
2019-04-13 08:38:32 +0800

    create redux store and added products page route

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -11,12 +11,14 @@
  */
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import Home from "./Home/components/Home";
 import Navbar from "./Elements/components/Navbar/Navbar";
 import About from "./About/components/About";
 import ProfileCard from "./Profile/components/ProfileCard";
 import ProfileForm from "./Profile/components/ProfileForm";
 import Weather from "./Weather/components/Weather";
+import Products from "./Products/components/Products.jsx";
 
 class App extends Component {
   render() {
@@ -30,6 +32,7 @@ class App extends Component {
             <Route exact path="/profile-card" component={ProfileCard} />
             <Route exact path="/profile-form" component={ProfileForm} />
             <Route exact path="/weather" component={Weather} />
+            <Route exact path="/products" component={Products} />
           </Switch>
         </div>
       </BrowserRouter>
